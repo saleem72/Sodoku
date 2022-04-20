52cfb46825f8e9f1f1a428e50207ede648588dd7
Nabil_saadi <nabil.saadi@codersdc.com>
2022-04-20 12:50:06 +0300

    Initial Commit

---
+++ Sodoku/SodokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SodokuApp.swift
+//  Sodoku
+//
+//  Created by Yousef on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SodokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
